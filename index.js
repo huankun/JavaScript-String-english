9cba999fb2d79ec2332b929b17ca16fa1298d23c
huankun <1446811802@qq.com>
2022-06-07 14:01:02 +0800

    Kun modify file

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase();
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+function replaceStr(str){
+    str = str.toLowerCase();
+    var reg = /\b(\w)|\s(\w)/g;
+    return str.replace(reg,function(m){ 
+     return m.toUpperCase()
+    });
+}
+sentence = replaceStr(sentence);
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = money.slice(1);
